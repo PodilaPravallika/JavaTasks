c045dac1218e864f31f9869f0c9061af830907bd
Podila Pravallika <111067180+PodilaPravallika@users.noreply.github.com>
2025-07-24 21:57:11 +0530

    Add files via upload

---
+++ Task2/Employee.java
@@ -0,0 +1,28 @@
+package tasks.oops;
+
+class EmployeeCount {
+    static int count = 0;
+
+    // Constructor increments count
+    EmployeeCount() {
+        count++;
+    }
+
+    // Getter method
+    static int getCount() {
+        return count;
+    }
+}
+
+public class Employee {
+    public static void main(String[] args) {
+        // Create multiple EmployeeCount objects
+        EmployeeCount e1 = new EmployeeCount();
+        EmployeeCount e2 = new EmployeeCount();
+        EmployeeCount e3 = new EmployeeCount();
+
+        // Print the total number of employees created
+        System.out.println("Total number of employees: " + EmployeeCount.getCount());
+    }
+}
+ 
