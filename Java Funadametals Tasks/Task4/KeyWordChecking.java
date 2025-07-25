db85332739b5ac376beb85841b8b3b09eedf4a1c
Podila Pravallika <111067180+PodilaPravallika@users.noreply.github.com>
2025-07-25 14:43:32 +0530

    Add files via upload

---
+++ Java Funadametals Tasks/Task4/KeyWordChecking.java
@@ -0,0 +1,8 @@
+package tasks;
+
+public class KeyWordChecking 
+{
+	public static void main(String[] args) {
+		// int class = 5;  // Compiler Error: 'class' is a reserved keyword
+	}
+}
---
+++ Java Funadametals Tasks/Task4/KeywordDemo.java
@@ -0,0 +1,14 @@
+package tasks;
+
+public class KeywordDemo {
+    public static void main(String[] args) {
+        final int x = 10;
+        if (x > 5) {
+            System.out.println("x is large");
+        }
+        // Add more keywords: class, public, static, void, int, if, etc.
+//        int class = 5;  // Compiler Error: 'class' is a reserved keyword
+    }
+}
+
+
