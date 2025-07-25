725f754e899c8362fae55b6747a35ec4ffc2db71
Podila Pravallika <111067180+PodilaPravallika@users.noreply.github.com>
2025-07-25 14:33:24 +0530

    Add files via upload

---
+++ Java Funadametals Tasks/Task3/NamePrinting.java
@@ -0,0 +1,8 @@
+package tasks;
+
+public class NamePrinting {
+public static void main(String[] args) {
+System.out.println("Hello, Pravallika!");
+System.out.println("Today is a great day to code Java!");
+}
+}
