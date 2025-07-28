4d86aabc2e51010a110ef6a794701d2a0e851039
Podila Pravallika <111067180+PodilaPravallika@users.noreply.github.com>
2025-07-28 07:18:27 +0530

    Add files via upload

---
+++ ExceptionHandling-Tasks/Task4/Main.java
@@ -0,0 +1,11 @@
+package com.school.main;
+
+import com.school.student.Student;
+
+public class Main {
+ public static void main(String[] args) {
+     Student s = new Student("Pravallika", 101);
+     s.displayStudentInfo();
+ }
+}
+
---
+++ ExceptionHandling-Tasks/Task4/Student.java
@@ -0,0 +1,20 @@
+package com.school.student;
+
+
+public class Student {
+ private String name;
+ private int rollNumber;
+
+ 
+ public Student(String name, int rollNumber) {
+     this.name = name;
+     this.rollNumber = rollNumber;
+ }
+
+ 
+ public void displayStudentInfo() {
+     System.out.println("Student Name: " + name);
+     System.out.println("Roll Number: " + rollNumber);
+ }
+}
+
